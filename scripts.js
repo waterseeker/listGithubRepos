d9093a0d34a3a037f71fa4e70fe7b01249af40e0
Wayne Burris <waterseeker@hotmail.com>
2017-02-05 09:12:48 -0600

    trying another setup

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$('document').ready(function() {
+    const endpoint = 'https://api.github.com/users/waterseeker/repos';
+    const repos = [];
+
+   $('button').click(function() {
+       $.get(endpoint, function(data) {
+           repos.push(...data);
+           console.log(repos);
+           for(var i = 0; i < repos.length; i++) {
+               $('#myRepos').append(
+                   `<li><a href="${repos[i].html_url}" target="_blank">
+                    ${repos[i].name}</a></li>`
+               );
+           }
+        }).fail(function() {
+            alert('Error! Could not load repositories');
+        });
+    });
+});
